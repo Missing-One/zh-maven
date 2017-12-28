025000532c74fce448dd079a3995b5e1cedfe675
Administrator <Administrator@JI7-16>
2017-12-28 17:25:08 +0800

    添加查询用户的借口

--- freight-logistics-system/src/main/java/com/groupsix/freightlogisticssystem/mapper/UserMapper.java
+++ freight-logistics-system/src/main/java/com/groupsix/freightlogisticssystem/mapper/UserMapper.java
@@ -14,4 +14,17 @@ public interface UserMapper {
     int updateByPrimaryKeySelective(User record);
 
     int updateByPrimaryKey(User record);
+    
+    
+    /**
+	 * 用户登陆
+	 * 需要登陆所需的账号密码
+	 * 
+	 * 	<li>账号,手机号{@link User.phone}
+	 *  <li>密码 {@link User.password}
+	 *  
+	 * @return 如果登陆成功,返回当前用户登陆的所有信息;如果登陆失败,返回 {@link null}
+	 */
+	User login(User user);
+    
 }
